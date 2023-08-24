8a2e32fffdae79b8389c5dba28bc95d3f3a1aca2
damodara123 <damodara.palacharla@ramanasoft.com>
2023-08-24 12:26:59 +0530

    Username and password methods are changes

--- src/main/java/com/Pages/nopCommerceLogin.java
+++ src/main/java/com/Pages/nopCommerceLogin.java
@@ -1,6 +1,7 @@
 package com.Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import com.Base.Browser;
 import com.Common.ExtentReport;
@@ -12,18 +13,21 @@ public class nopCommerceLogin extends Browser{
 	
 	public By password = By.xpath("//input[@name='Password']");
 	
-	public By login = By.xpath("//button[text()='Login']");
+	public By login = By.xpath("//button[text()='Log in']");
 	
 	
 	
 	
 	public boolean enterEmail()
 	{
+		clickElement(username);
+		driver.findElement(username).sendKeys(Keys.CONTROL,"a",Keys.DELETE);
 		return enterText(username,"admin@yourstore.com");
 	}
 	public boolean enterPassword()
 	{
-		return enterText(password,"adminadmin");
+		textClear(password);
+		return enterText(password,"admin");
 	}
 	
 	public boolean userLogin() 
